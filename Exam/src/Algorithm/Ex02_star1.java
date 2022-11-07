481283cdfd3b6014b70290a3ace55ae0f3147eb3
lsa05 <ssangah05@gmail.com>
2022-11-07 22:01:07 +0900

    스터디 1일 (수정)

--- Exam/src/Algorithm/Ex02_star1.java
+++ Exam/src/Algorithm/Ex02_star1.java
@@ -3,7 +3,7 @@ package Algorithm;
 public class Ex02_star1 {
 	public static void main(String[] args) {
 		
-		//왼쪽으로 쏠린 별
+		//1.왼쪽으로 쏠린 별
         for (int i = 1; i <= 6; i++) {
             for (int j = 1; j <=i; j++) {
                 System.out.print("*");
@@ -12,7 +12,7 @@ public class Ex02_star1 {
         }       
         System.out.println();
 
-		//오른쪽으로 쏠린 별
+		//2.오른쪽으로 쏠린 별
 		for(int i=1; i<=6; i++) {
 			for(int j=1; j<=(6-i); j++) { //공백
 					System.out.print(" ");
@@ -23,7 +23,7 @@ public class Ex02_star1 {
 		}			
 		System.out.println();
 			
-		//피라미드형 별
+		//3.피라미드형 별
 		for(int i=1; i<=6; i++) {
 			for(int j=1; j<=(6-i); j++) {
 				System.out.print(" ");
@@ -34,7 +34,31 @@ public class Ex02_star1 {
 		}
 		System.out.println();
 
-
+		/* 팀원들 풀이
+		 * 2번. 
+		 * for(int i=0; i<7; i++){
+		 * 	for(int j=0;j<6;j++){
+		 * 	  if(j+i>=6){
+		 * 		System.out.print("*");
+		 *    }else{
+		 *  }System.out.println();
+		 * } 
+		 * 
+		 * 
+		 * 
+		 * 2번
+		 * for(int i=1; i<=6; i++){
+		 *  for(int k=6-i; k>0; k--){
+		 *   System.out.print(" ");
+		 *  }
+		 *  for(int j=1; j<=i; j++){
+		 *   System.ouot.print("*");
+		 *  }
+		 *  System.out.println();
+		 * }  
+		 * */
+		
+		
 	}   
 
 }
